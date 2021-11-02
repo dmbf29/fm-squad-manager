e2e1f96f7b6e8de50ec32e81c8a68c7b980b0e40
Douglas Berkley <douglasmberkley@gmail.com>
2021-11-02 14:37:54 +0900

    Added authorization to squads

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -1,28 +1,24 @@
 class SquadsController < ApplicationController
   before_action :set_squad, only: [:show, :edit, :update, :destroy]
 
-  # GET /squads
   def index
-    @squads = Squad.all
+    @squads = policy_scope(Squad)
   end
 
-  # GET /squads/1
   def show
   end
 
-  # GET /squads/new
   def new
     @squad = Squad.new
+    authorize @squad
   end
 
-  # GET /squads/1/edit
   def edit
   end
 
-  # POST /squads
   def create
     @squad = Squad.new(squad_params)
-
+    authorize @squad
     if @squad.save
       redirect_to @squad, notice: 'Squad was successfully created.'
     else
@@ -30,7 +26,6 @@ class SquadsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /squads/1
   def update
     if @squad.update(squad_params)
       redirect_to @squad, notice: 'Squad was successfully updated.'
@@ -39,20 +34,18 @@ class SquadsController < ApplicationController
     end
   end
 
-  # DELETE /squads/1
   def destroy
     @squad.destroy
     redirect_to squads_url, notice: 'Squad was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_squad
-      @squad = Squad.find(params[:id])
-    end
+  def set_squad
+    @squad = Squad.find(params[:id])
+    authorize @squad
+  end
 
-    # Only allow a list of trusted parameters through.
-    def squad_params
-      params.require(:squad).permit(:name, :user_id)
-    end
+  def squad_params
+    params.require(:squad).permit(:name, :user_id)
+  end
 end
